67493209261379ab5c9afbfaab95550bc7d2cdb9
Sukumar Ponram <suku8u@uw.edu>
2017-06-06 21:58:49 -0700

    Made change to action method to be of ActionResult type

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,16 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public String Detail()
+        public ActionResult Detail()
         {
-            return "Hello from the comics book controller";
-        }
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return  Redirect("/");
+            };
+
+            return Content("Hello from the comics book controller");
+
+
+         }
     }
 }
